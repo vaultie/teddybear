09197a9e6b9c7bf812b1d8e3138f816b66bd6c0c
Ivan Leshchenko <ivan@ivan770.me>
2024-03-12 14:53:26 -0400

    feat: JWS verification function (#27)

--- tests/src/ed25519.test.ts
+++ tests/src/ed25519.test.ts
@@ -1,5 +1,5 @@
-import { PrivateEd25519 } from '@vaultie/teddybear-node'
-import { JWK, compactVerify, importJWK } from 'jose'
+import { PrivateEd25519, verifyJWS } from '@vaultie/teddybear-node'
+import { CompactSign, JWK, compactVerify, exportJWK, generateKeyPair, importJWK } from 'jose'
 import { describe, it, expect } from 'vitest'
 
 describe('can execute common private key operations', () => {
@@ -23,11 +23,49 @@ describe('can execute common private key operations', () => {
     key.toX25519PrivateJWK()
   })
 
-  it('can sign jws values', async () => {
+  it('can sign JWS values', async () => {
     const key = await PrivateEd25519.generate()
 
     const jws = key.signJWS('testvalue')
     const { payload } = await compactVerify(jws, await importJWK(key.toEd25519PublicJWK().toJSON() as JWK))
     expect(new TextDecoder().decode(payload)).toStrictEqual('testvalue')
+  })
+
+  it('can extract JWS payload', async () => {
+    const key = await PrivateEd25519.generate()
+
+    const jws = key.signJWS('testvalue')
+
+    const { jwk, payload } = verifyJWS(jws)
+    expect(jwk.toJSON()).toStrictEqual(key.toEd25519PublicJWK().toJSON())
+    expect(new TextDecoder().decode(payload)).toStrictEqual('testvalue')
+  })
+
+  it('can use valid JWS values from other libraries', async () => {
+    const { publicKey, privateKey } = await generateKeyPair('EdDSA')
+    const publicKeyJWK = await exportJWK(publicKey)
+
+    const jws = await new CompactSign(new TextEncoder().encode('Hello, world'))
+      .setProtectedHeader({ alg: 'EdDSA', jwk: publicKeyJWK })
+      .sign(privateKey)
+
+    const { jwk, payload } = verifyJWS(jws)
+    expect(jwk.toJSON()).toStrictEqual(publicKeyJWK)
+    expect(new TextDecoder().decode(payload)).toStrictEqual('Hello, world')
+  })
+
+  it('can reject JWS values with invalid embedded JWKs from other libraries', async () => {
+    const { privateKey } = await generateKeyPair('EdDSA')
+    const { publicKey } = await generateKeyPair('EdDSA')
+
+    const jws = await new CompactSign(new TextEncoder().encode('Hello, world'))
+      .setProtectedHeader({ alg: 'EdDSA', jwk: await exportJWK(publicKey) })
+      .sign(privateKey)
+
+    expect(() => verifyJWS(jws)).toThrow()
+  })
+
+  it('can reject invalid JWS values', async () => {
+    expect(() => verifyJWS('123')).toThrow()
   })
 })
