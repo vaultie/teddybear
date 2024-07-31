86fab9b46b83534f5b78e38ac94a9e4b90bf8f1e
Ivan Leshchenko <ivan@ivan770.me>
2024-07-31 12:15:31 -0400

    feat: C2PA PDF support (#56)

--- tests/src/c2pa.test.ts
+++ tests/src/c2pa.test.ts
@@ -3,17 +3,18 @@ import { readFileSync } from 'fs'
 import { describe, it } from 'vitest'
 
 const image = readFileSync(process.env.placeholderImage!)
+const pdf = readFileSync(process.env.placeholderPdf!)
 const certificate = readFileSync(process.env.certificate!)
 
 describe('can execute C2PA operations', () => {
-  it('can sign a C2PA manifest', async () => {
+  it('can sign an image', async () => {
     // This key should correspond to the certificate private key
     const keyBytes = Buffer.from('5ff5e2393a44256abe197c82742366ff2f998f6822980e726f8fd16d6bd07eb1', 'hex')
 
     const key = await PrivateEd25519.fromBytes(new Uint8Array(keyBytes))
 
     key.embedC2PAManifest(new Uint8Array(certificate), new Uint8Array(image), 'image/jpeg', {
-      title: 'Hello World',
+      title: 'Test Image',
       assertions: [
         {
           label: 'stds.schema-org.CreativeWork',
@@ -24,9 +25,28 @@ describe('can execute C2PA operations', () => {
           },
           kind: 'Json'
         }
-      ],
-      claim_generator_info: [
-        { name: 'Teddybear' }
+      ]
+    })
+  })
+
+  it('can sign a PDF file', async () => {
+    // This key should correspond to the certificate private key
+    const keyBytes = Buffer.from('5ff5e2393a44256abe197c82742366ff2f998f6822980e726f8fd16d6bd07eb1', 'hex')
+
+    const key = await PrivateEd25519.fromBytes(new Uint8Array(keyBytes))
+
+    key.embedC2PAManifest(new Uint8Array(certificate), new Uint8Array(pdf), 'application/pdf', {
+      title: 'Test PDF',
+      assertions: [
+        {
+          label: 'stds.schema-org.CreativeWork',
+          data: {
+            '@context': 'http://schema.org/',
+            '@type': 'CreativeWork',
+            url: 'https://example.com'
+          },
+          kind: 'Json'
+        }
       ]
     })
   })
