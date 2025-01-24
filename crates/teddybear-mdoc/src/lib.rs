dd5b7eca0038722564bbdce0c21dd1709499abbb
ivan770 <ivan@ivan770.me>
2025-01-24 10:35:20 -0500

    Improve and extend the MDoc API

--- crates/teddybear-mdoc/src/lib.rs
+++ crates/teddybear-mdoc/src/lib.rs
@@ -5,16 +5,17 @@ use isomdl::{
         device_request::{self, ItemsRequest},
         helpers::{ByteStr, Tag24},
         x509::{trust_anchor::TrustAnchorRegistry, X5Chain},
-        CoseKey, DeviceKeyInfo, DigestAlgorithm, EC2Curve, SessionEstablishment, ValidityInfo,
-        EC2Y,
+        CoseKey, DeviceKeyInfo, DigestAlgorithm, EC2Curve, SessionEstablishment, EC2Y,
     },
     issuance::{self, Mdoc, Namespaces},
-    presentation::device::{self, DeviceSession, SessionManagerInit},
+    presentation::device::{self, DeviceSession, Document, SessionManager, SessionManagerInit},
 };
 use p256::ecdsa::{signature::Signer, Signature};
 use teddybear_crypto::{EcdsaSecp256r1VerificationKey2019, PrivateSecp256r1, ToEncodedPoint};
 use thiserror::Error;
 use time::OffsetDateTime;
+
+pub use isomdl::definitions::ValidityInfo;
 
 #[derive(Error, Debug)]
 pub enum Error {
@@ -116,95 +117,131 @@ impl Default for MDocBuilder {
     }
 }
 
-pub fn namespaces(
-    document: &[u8],
-) -> Result<BTreeMap<String, BTreeMap<String, ciborium::Value>>, Error> {
-    let mdoc: Mdoc = isomdl::cbor::from_slice(document)?;
+pub struct DeviceInternalMDoc(Document);
 
-    Ok(mdoc
-        .namespaces
-        .into_inner()
-        .into_iter()
-        .map(|(name, entries)| {
-            let value = entries
-                .into_inner()
-                .into_iter()
-                .map(|v| {
-                    let item = v.into_inner();
-                    (item.element_identifier, item.element_value)
-                })
-                .collect();
-
-            (name, value)
-        })
-        .collect())
-}
-
-pub fn present<D, R>(
-    device_key: &PrivateSecp256r1,
-    verifier_key: &EcdsaSecp256r1VerificationKey2019,
-    documents: D,
-    requests: R,
-    permits: BTreeMap<String, BTreeMap<String, Vec<String>>>,
-) -> Result<Vec<u8>, Error>
-where
-    D: IntoIterator<Item = (String, Vec<u8>)>,
-    R: IntoIterator<Item = (String, device_request::Namespaces)>,
-{
-    let verifier_key_point = verifier_key.public_key.decoded().to_encoded_point(false);
-
-    let x = verifier_key_point
-        .x()
-        .ok_or(Error::MissingXCoordinate)?
-        .to_vec();
-
-    let y = verifier_key_point
-        .y()
-        .ok_or(Error::MissingYCoordinate)?
-        .to_vec();
-
-    let documents = documents
-        .into_iter()
-        .map(|(name, value)| {
-            let mdoc: Mdoc = isomdl::cbor::from_slice(&value)?;
-            Ok((name, mdoc.into()))
-        })
-        .collect::<Result<BTreeMap<_, device::Document>, Error>>()?;
-
-    let (session, _) = SessionManagerInit::initialise(documents.try_into().unwrap(), None, None)?
-        .qr_engagement()?
-        .0
-        .process_session_establishment(
-            SessionEstablishment {
-                e_reader_key: Tag24::new(CoseKey::EC2 {
-                    crv: EC2Curve::P256,
-                    x,
-                    y: EC2Y::Value(y),
-                })
-                .unwrap(),
-                data: ByteStr::from(vec![]),
-            },
-            TrustAnchorRegistry::from_pem_certificates(vec![])?,
-        )?;
-
-    let mut device_response = session.prepare_response(
-        &requests
-            .into_iter()
-            .map(|(doc_type, namespaces)| ItemsRequest {
-                doc_type,
-                namespaces,
-                request_info: None,
-            })
-            .collect(),
-        permits,
-    );
-
-    while let Some((_, payload)) = device_response.get_next_signature_payload() {
-        let signature: Signature = device_key.ecdsa_signing_key().sign(payload);
-        device_response.submit_next_signature(signature.to_bytes().to_vec());
+impl DeviceInternalMDoc {
+    pub fn from_bytes(value: &[u8]) -> Result<Self, Error> {
+        let document = isomdl::cbor::from_slice(value)?;
+        Ok(Self(document))
     }
 
-    let finalized_response = device_response.finalize_response();
+    pub fn from_issued_bytes(value: &[u8]) -> Result<Self, Error> {
+        let mdoc: Mdoc = isomdl::cbor::from_slice(value)?;
+        Ok(Self(mdoc.into()))
+    }
 
-    Ok(isomdl::cbor::to_vec(&finalized_response)?)
+    pub fn doc_type(&self) -> &str {
+        &self.0.mso.doc_type
+    }
+
+    pub fn namespaces(&self) -> BTreeMap<String, BTreeMap<String, ciborium::Value>> {
+        self.0
+            .namespaces
+            .iter()
+            .map(|(namespace, entries)| {
+                let value = entries
+                    .iter()
+                    .map(|(_, value)| {
+                        let value = value.clone().into_inner();
+                        (value.element_identifier, value.element_value)
+                    })
+                    .collect();
+
+                (namespace.clone(), value)
+            })
+            .collect()
+    }
+
+    pub fn validity_info(&self) -> &ValidityInfo {
+        &self.0.mso.validity_info
+    }
+
+    pub fn to_bytes(&self) -> Result<Vec<u8>, Error> {
+        Ok(isomdl::cbor::to_vec(&self.0)?)
+    }
+}
+
+pub struct PendingPresentation {
+    session: SessionManager,
+}
+
+impl PendingPresentation {
+    pub fn start<D>(
+        verifier_key: &EcdsaSecp256r1VerificationKey2019,
+        trust_anchor_registry: TrustAnchorRegistry,
+        documents: D,
+    ) -> Result<Self, Error>
+    where
+        D: IntoIterator<Item = (String, Vec<u8>)>,
+    {
+        let verifier_key_point = verifier_key.public_key.decoded().to_encoded_point(false);
+
+        let x = verifier_key_point
+            .x()
+            .ok_or(Error::MissingXCoordinate)?
+            .to_vec();
+
+        let y = verifier_key_point
+            .y()
+            .ok_or(Error::MissingYCoordinate)?
+            .to_vec();
+
+        let documents = documents
+            .into_iter()
+            .map(|(name, value)| {
+                let mdoc: Mdoc = isomdl::cbor::from_slice(&value)?;
+                Ok((name, mdoc.into()))
+            })
+            .collect::<Result<BTreeMap<_, device::Document>, Error>>()?;
+
+        let (session, _) =
+            SessionManagerInit::initialise(documents.try_into().unwrap(), None, None)?
+                .qr_engagement()?
+                .0
+                .process_session_establishment(
+                    SessionEstablishment {
+                        e_reader_key: Tag24::new(CoseKey::EC2 {
+                            crv: EC2Curve::P256,
+                            x,
+                            y: EC2Y::Value(y),
+                        })
+                        .unwrap(),
+                        data: ByteStr::from(vec![]),
+                    },
+                    trust_anchor_registry,
+                )?;
+
+        Ok(Self { session })
+    }
+
+    pub fn consent<R>(
+        self,
+        device_key: &PrivateSecp256r1,
+        requests: R,
+        permits: BTreeMap<String, BTreeMap<String, Vec<String>>>,
+    ) -> Result<Vec<u8>, Error>
+    where
+        R: IntoIterator<Item = (String, device_request::Namespaces)>,
+    {
+        let mut device_response = self.session.prepare_response(
+            &requests
+                .into_iter()
+                .map(|(doc_type, namespaces)| ItemsRequest {
+                    doc_type,
+                    namespaces,
+                    request_info: None,
+                })
+                .collect(),
+            permits,
+        );
+
+        while let Some((_, payload)) = device_response.get_next_signature_payload() {
+            let signature: Signature = device_key.ecdsa_signing_key().sign(payload);
+            device_response.submit_next_signature(signature.to_bytes().to_vec());
+        }
+
+        let finalized_response = device_response.finalize_response();
+
+        Ok(isomdl::cbor::to_vec(&finalized_response)?)
+    }
 }
