8aa2dde3a2e53336c1cb5c04dbc1be155727bcad
Ivan Leshchenko <ivan@ivan770.me>
2025-02-18 13:17:58 -0500

    Resolve IssuerSigned in fromIssuedBytes (#112)
    * Resolve IssuerSigned in fromIssuedBytes
    
    * Fix CI

--- nix/wasm-snip.nix
+++ nix/wasm-snip.nix
@@ -14,7 +14,8 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-/VdtcpvrSodgu1RK+IQEabCxsRkWjurCcA5LEgxgZ/k=";
   };
 
-  cargoHash = "sha256-hYFqhEMzfmElBk4S6f8SEMQAwHVAS+jbJpGMEXi8Jyk=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-XdN3xK6BxFDg7Vb+EgEYY2bGCMW8J9oFns579pQyIb8=";
 
   # Tests require cargo-readme
   doCheck = false;
